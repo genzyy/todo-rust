0d7be9d807d03d63c8496a74d599742e38838e14
Rishit Pandey <rishpandey8097@gmail.com>
2021-03-25 20:17:55 +0530

    final code files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use std::collections::HashMap;
-use std::io::Read;
-use std::str::FromStr;
 
 struct Todo {
     map: HashMap<String, bool>,
@@ -23,13 +21,15 @@ impl Todo {
      * in a local .txt file saved as "db.txt".
      */
 
-    fn save(self) -> Result<(), std::io::Error> {
-        let mut content = String::new();
-        for (k, v) in self.map {
-            let record = format!("{}\t{}\n", k, v);
-            content.push_str(&record);
-        }
-        return std::fs::write("db.txt", content);
+    fn save(self) -> Result<(), Box<dyn std::error::Error>> {
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open("db.json")?;
+        serde_json::to_writer_pretty(f, &self.map)?;
+
+        Ok(())
     }
 
     /**
@@ -55,21 +55,26 @@ impl Todo {
      */
 
     fn new() -> Result<Todo, std::io::Error> {
-        let mut f = std::fs::OpenOptions::new()
+        let f = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
             .read(true)
-            .open("db.txt")?;
+            .open("db.json")?;
 
-        let mut content = String::new();
-        f.read_to_string(&mut content)?;
-        let map: HashMap<String, bool> = content
-            .lines()
-            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
-            .map(|v| (v[0], v[1]))
-            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
-            .collect();
-        return Ok(Todo { map });
+        match serde_json::from_reader(f) {
+            Ok(map) => Ok(Todo { map }),
+            Err(e) if e.is_eof() => Ok(Todo {
+                map: HashMap::new(),
+            }),
+            Err(e) => panic!("An error occurred: {}", e),
+        }
+    }
+
+    fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(v) => Some(*v = true),
+            None => None,
+        }
     }
 }
 
@@ -88,15 +93,19 @@ fn main() {
 
     let mut todo = Todo::new().expect("Initialization of db failed somehow");
 
-    let mut todo = Todo {
-        map: HashMap::new(),
-    };
-
     if action == "add" {
         todo.insert(item);
         match todo.save() {
             Ok(_) => println!("Task saved successfully!"),
             Err(why) => println!("An error occurred: {}", why),
+        }
+    } else if action == "complete" {
+        match todo.complete(&item) {
+            None => println!("The given task: {} not found!", item),
+            Some(_) => match todo.save() {
+                Ok(_) => println!("todo saved"),
+                Err(why) => println!("An error occurred: {}", why),
+            },
         }
     }
 }

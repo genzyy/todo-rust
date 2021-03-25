06a55042b58bbe1cd77d57d6b5b24ec20b04d6e8
Rishit Pandey <rishpandey8097@gmail.com>
2021-03-25 17:44:37 +0530

    insert and save

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,21 @@ impl Todo {
     fn insert(&mut self, key: String) {
         self.map.insert(key, false);
     }
+
+    /**
+     * The -> means the return type of the function that we use here.
+     * We save the contents of the tasks as task: completed status
+     * in a local .txt file saved as "db.txt".
+     */
+
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record: str = format!("{}\t{}\n", k, v);
+            content.push_str(&record);
+        }
+        return std::fs::write("db.txt", content);
+    }
 }
 
 fn main() {
@@ -28,6 +43,18 @@ fn main() {
         .expect("Please specify an action here!");
 
     let item = std::env::args().nth(2).expect("Please specify an item");
+
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("Task saved successfully!"),
+            Err(why) => println!("An error occurred: {}", why),
+        }
+    }
 
     println!("{:?} {:?}", action, item);
 }
